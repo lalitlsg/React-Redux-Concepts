544d8cca70dc22e6479b26752b4e383bc1c2e597
Lalit Garghate <lalit.garghate@gmail.com>
2020-11-13 19:49:25 +0530

    useDispatch Hook

--- src/Components/HooksCoffeeContainer.js
+++ src/Components/HooksCoffeeContainer.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { buyCoffee } from "../Redux";
 
 function HooksCoffeeContainer() {
   const coffeeQty = useSelector(state => state.coffeeQty);
+  const dispatch = useDispatch();
   return (
     <div>
       <h2>Hooks Coffee Quantity: {coffeeQty}</h2>
-      <button>Buy Coffee</button>
+      <button onChange={() => dispatch(buyCoffee)}>Buy Coffee</button>
     </div>
   );
 }
