0844fbfa5ddf7811417140c271c088f8fe26f1e3
Lalit Garghate <lalit.garghate@gmail.com>
2020-11-13 18:27:32 +0530

    Created Coffee Component

---
+++ src/Components/Coffee.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Coffee() {
+  return (
+    <div>
+      <h2>Coffee Quantity</h2>
+      <button>Buy Coffee</button>
+    </div>
+  );
+}
+
+export default Coffee;
